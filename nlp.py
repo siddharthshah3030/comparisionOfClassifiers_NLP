aa30602f59c34dfd7f77ee9936d09a7bc3c97993
Siddharth Shah <siddharthshah3030@gmail.com>
2018-12-05 02:06:51 +0530

    complete cleaning

--- nlp.py
+++ nlp.py
@@ -9,8 +9,11 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0])
-review = review.lower()
-review = review.split()
-ps = PorterStemmer
-review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] 
+corpus = [0]
+for i in range(0,1000):
+    
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] 
