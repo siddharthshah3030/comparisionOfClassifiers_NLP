915c1860ed4c501ab94c5d8e294cda2a9f200d3c
Siddharth Shah <siddharthshah3030@gmail.com>
2018-12-05 01:38:27 +0530

    cleaning the review string making array of useful words with librarie

--- nlp.py
+++ nlp.py
@@ -4,5 +4,13 @@ import pandas as pd
 
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
 
-
-
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0])
+review = review.lower()
+review = review.split()
+ps = PorterStemmer
+review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] 
