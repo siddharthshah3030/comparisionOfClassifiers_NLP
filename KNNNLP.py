a5ef296220ce46c72f344ff4223f356df4a77341
Siddharth Shah <siddharthshah3030@gmail.com>
2018-12-05 18:56:58 +0530

    adding KNN for classification

---
+++ KNNNLP.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
+
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] 
+    review = ' '.join(review)
+    corpus.append(review)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values 
+
+    
+
+#73% accuracy is found here 
