8a7e9d20c36322bdec6ef67083cc21d4db543c76
Siddharth Shah <siddharthshah3030@gmail.com>
2018-12-05 21:00:01 +0530

    calling all classifiers in main

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import pandas as pd
 
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
 
+def printf(cm):
+    TP = cm[1][1]
+    FN = cm[0][1]
+    FP = cm[1][0]
+    TN = cm[0][0]
+    Accuracy = (TP + TN) / (TP + TN + FP + FN)
+    Precision = TP / (TP + FP)
+    Recall = TP / (TP + FN)
+    F1_score = 2 * Precision * Recall / (Precision + Recall)
+    print('accuracy =', Accuracy, ' ,precision =',Precision,' ,Recall =', Recall, ' F1Score ' ,F1_score)
+
+#cleaning 
 import re
 import nltk
 nltk.download('stopwords')
@@ -26,3 +38,24 @@ y = dataset.iloc[:,1].values
 
 import KNNNLP
 %run KNNNLP.py
+printf(cm)
+
+import decisiontreeNLP
+%run decisionTreeNLP.py
+printf(cm)
+
+import NaivesBayesNLP
+%run NaivesBayesNLP.py
+printf(cm)
+
+import SGDNLP
+%run KNNNLP.py
+printf(cm)
+
+import KNNNLP
+%run KNNNLP.py
+printf(cm)
+
+import KNNNLP
+%run KNNNLP.py
+printf(cm)
