708dc52d712c8b72aa89a1b2f985cc51f3086b5f
Siddharth Shah <siddharthshah3030@gmail.com>
2018-12-05 19:56:19 +0530

    adding SDG classifier

---
+++ SGDNLP.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
+
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] 
+    review = ' '.join(review)
+    corpus.append(review)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values 
+
+    
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,random_state = 0)
+
+
+# Fitting SVM classifier to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
