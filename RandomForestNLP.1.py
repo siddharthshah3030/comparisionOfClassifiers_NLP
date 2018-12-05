4fec860ae3df9845ab4fc5a499ddf86196acd9e2
Siddharth Shah <siddharthshah3030@gmail.com>
2018-12-05 19:01:26 +0530

    adding random forest

---
+++ RandomForestNLP.1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting = 3)
+
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))] 
+    review = ' '.join(review)
+    corpus.append(review)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values 
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Fitting Random Forest classifier to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=2, criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
